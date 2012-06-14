44b99e31ed1591e78ad70f16faf8c5e90058d2bd
pvdvreede <paul@vdvreede.net>
2012-06-14 17:27:54 +1000

    Add setup for pypi dist.

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from distutils.core import setup
+
+setup(
+        name='page-changer',
+        author='Paul Van de Vreede',
+        author_email='paul@vdvreede.net',
+        version='0.9.0',
+        py_modules=['page-changer'],
+     )
