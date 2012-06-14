8e7e9af1491b0247271806609eff2616662b750d
pvdvreede <paul@vdvreede.net>
2012-06-14 17:56:17 +1000

    Add to setup file.

--- setup.py
+++ setup.py
@@ -5,6 +5,13 @@ setup(
         name='page-changer',
         author='Paul Van de Vreede',
         author_email='paul@vdvreede.net',
+        description='File alteration bot for programmatically altering large amount of files with Regular Expressions.',
         version='0.9.0',
+        url='https://github.com/pvdvreede/page-changer',
         py_modules=['page-changer'],
+        entry_points={
+            'console_scripts': [
+                'page-changer = page-changer:main',
+                ],
+            }
      )
