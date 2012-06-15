b41fc6a470838b5b0b20d4c4bf2c96183a039640
pvdvreede <paul@vdvreede.net>
2012-06-16 00:05:54 +1000

    Rename setup for - removal.

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@
 from distutils.core import setup
 
 setup(
-        name='page-changer',
+        name='pagechanger',
         author='Paul Van de Vreede',
         author_email='paul@vdvreede.net',
         description='File alteration bot for programmatically altering large amount of files with Regular Expressions.',
         version='0.9.0',
         url='https://github.com/pvdvreede/page-changer',
-        py_modules=['page-changer'],
+        py_modules=['pagechanger'],
         entry_points={
             'console_scripts': [
-                'page-changer = page-changer:main',
+                'pagechanger = pagechanger:main',
                 ],
             }
      )
