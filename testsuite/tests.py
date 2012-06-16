3d830ca4b83bdc13943303eeec480bdf362b5df6
pvdvreede <paul@vdvreede.net>
2012-06-16 11:21:12 +1000

    Add initial tests.

---
+++ testsuite/tests.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+
+import unittest
+import sys
+import os
+path1 = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(path1)
+import pagechanger
+
+
+class PageChangerTests(unittest.TestCase):
+    def setUp(self):
+        self.config = {
+                'exceptions' : ['testfile1.txt'],
+                'name': 'text files',
+                'mask': '*.txt',
+                'remove': [
+                    'text',
+                    'colou*r'
+                    ]
+                }
+        self.test_path = os.path.dirname(__file__)
+
+    def test_exceptions(self):
+        self.assertTrue(pagechanger.is_exception('testfile1.txt', self.config))
+        self.assertFalse(pagechanger.is_exception('testfile2.txt', self.config))
+
+    def test_get_files_recursive(self):
+        actual_files = pagechanger.get_files(self.test_path, self.config['mask'], True, self.config)
+        self.assertEqual(3, len(actual_files))
+
+
+if __name__ == '__main__':
+    unittest.main()
